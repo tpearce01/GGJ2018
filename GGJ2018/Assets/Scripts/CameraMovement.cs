075dd2590d90798d229f3d3e5fba94ed225b200d
Tyler Pearce <pearcetyler@yahoo.com>
2018-01-26 20:54:00 -0800

    Camera + Player Movement Prototypes

---
+++ GGJ2018/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+	[SerializeField] GameObject player;
+
+	void Update(){
+		Move ();
+	}
+
+	void Move(){
+		if (gameObject.transform.position.x < player.transform.position.x) {
+			//Move towards the player
+			gameObject.transform.position = new Vector3(player.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
+		} else {
+			//Don't move
+		}
+	}
+}
---
+++ GGJ2018/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+	[SerializeField] float moveSpeed;
+	[SerializeField] Rigidbody2D rgbd;
+
+	void Update(){
+		if (Input.GetKey (KeyCode.A)) {
+			Move (Direction.Left);
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			Move (Direction.Right);
+		}
+	}
+
+	void Move(Direction d){
+		rgbd.MovePosition ((Vector2)gameObject.transform.position + (int)d * Vector2.right * moveSpeed * Time.deltaTime);
+	}
+}
+
+public enum Direction{
+	Left = -1,
+	Right = 1
+}
