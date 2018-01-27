85434ba03e730f3226d1075e799f3879212de58c
Tyler Pearce <pearcetyler@yahoo.com>
2018-01-27 15:37:20 -0800

    Added helper function xPosition

--- GGJ2018/Assets/Scripts/CameraMovement.cs
+++ GGJ2018/Assets/Scripts/CameraMovement.cs
@@ -18,9 +18,14 @@ public class CameraMovement : MonoBehaviour {
 	void Move(){
 		if (gameObject.transform.position.x < player.transform.position.x) {
 			//Move towards the player
-			gameObject.transform.position = new Vector3(player.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
+			gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, new Vector3(player.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z), 0.1f);
+			//gameObject.transform.position = new Vector3(player.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
 		} else {
 			//Don't move
 		}
+	}
+
+	public static int xPosition(){
+		return camera.gameObject.transform.position.x;
 	}
 }
