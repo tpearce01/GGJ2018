2f6c5195cba6abf540b4fb11f82b5b6ced48ca4f
Tyler Pearce <pearcetyler@yahoo.com>
2018-01-27 16:20:40 -0800

    Fixed Camera and Player movement
    - Changed movement to FixedUpdate, which removed jitter
    - Added doge spritesheet
    - Created idle animation
    - Created walking animatino
    - Improved camera follow script
    - Added animation variables to PlayerMovement

--- GGJ2018/Assets/Scripts/CameraMovement.cs
+++ GGJ2018/Assets/Scripts/CameraMovement.cs
@@ -6,19 +6,20 @@ public class CameraMovement : MonoBehaviour {
 
 	public static GameObject camera;
 	[SerializeField] GameObject player;
+	[SerializeField] float offset;
 
 	void Start(){
 		camera = this.gameObject;
 	}
 
-	void Update(){
+	void FixedUpdate(){
 		Move ();
 	}
 
 	void Move(){
-		if (gameObject.transform.position.x < player.transform.position.x) {
+		if (gameObject.transform.position.x < player.transform.position.x + offset) {
 			//Move towards the player
-			gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, new Vector3(player.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z), 0.1f);
+			gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, new Vector3(player.transform.position.x + offset, gameObject.transform.position.y, gameObject.transform.position.z), 0.1f);
 			//gameObject.transform.position = new Vector3(player.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
 		} else {
 			//Don't move
